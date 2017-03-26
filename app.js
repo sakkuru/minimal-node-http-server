1bb300ca3a2dfb173b905aebaaa0414ed99578c0
sakkuru <sakkuru0323@gmail.com>
2017-03-26 20:19:10 +0900

    display requested path

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
+  console.log('requested path:', req.url)
   res.write('Hello World!');
   res.end();
 });
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
-  console.log('listening on port', port);
+  console.log('listening on port:', port);
 });
