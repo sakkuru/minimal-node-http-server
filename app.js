ee39c405c97f0ab8397915aeab1157c58771d05a
sakkuru <sakkuru0323@gmail.com>
2017-04-24 19:15:11 +0900

    fix app name

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  console.log('requested path:', req.url)
+  console.log('requested path:', req.url);
   res.write('Hello World!');
   res.end();
 });
